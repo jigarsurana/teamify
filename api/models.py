86b03843a8853f6a60ce4156fb2185a7409925c0
Piyush <piyushnahar1991@gmail.com>
2015-10-03 05:46:23 -0700

    Modify api py files

--- api/models.py
+++ api/models.py
@@ -3,12 +3,20 @@ from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 
 # Create your models here.
+class YourSkills(models.Model):
+	skill_name = models.CharField(max_length=200)
+
+class YourExpertise(models.Model):
+	expertise_name = models.CharField(max_length=200)
+
+class ReqSkills(models.Model):
+	skill_name = models.CharField(max_length=200)
+
+class ReqExpertise(models.Model):
+	expertise_name = models.CharField(max_length=200)
+
 class Person(models.Model):
 	email_id = models.EmailField(max_length=80, primary_key=True)
 	name = models.CharField(max_length=200)
-	phone_regex = RegexValidator(regex=r'^\+?\d{10,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone_number = models.CharField(max_length=15)
-	google_id = models.CharField(max_length=200, blank=True)
-	facebook_id = models.CharField(max_length=200, blank=True)
-	gender = models.CharField(max_length=1, blank=True)
-	city= models.CharField(max_length=51, blank=True)
+	your_skills = models.ManyToManyField(YourSkills)
--- api/serializer.py
+++ api/serializer.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 # from django.forms import widgets
 from api.models import *
 
+class YourSkillSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = YourSkills
+		fields = ("skill_name")
+
 class PersonSerializer(serializers.ModelSerializer):
+    your_skills = YourSkillSerializer(many= True, required = False)
     class Meta:
         model = Person
-        fields = ('email_id', 'name', 'city','phone_number')
+        fields = ('email_id', 'name', 'phone_number', 'your_skills')
